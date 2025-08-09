01105af3e20beb296886defb7c09c9b2a2165967
chemintsai <r10945043@ntu.edu.tw>
2025-08-09 15:33:36 +0800

    Performance tweak: enforce static inline

--- math-toolkit.h
+++ math-toolkit.h
@@ -5,7 +5,11 @@
 #include <stdio.h>
 #include <assert.h>
 
-static inline
+#ifndef STATIC_INLINE
+#define STATIC_INLINE static inline __attribute__((always_inline))
+#endif
+
+STATIC_INLINE
 void normalize(double *v)
 {
     double d = sqrt(v[0] * v[0] + v[1] * v[1] + v[2] * v[2]);
@@ -16,41 +20,41 @@ void normalize(double *v)
     v[2] /= d;
 }
 
-static inline
+STATIC_INLINE
 double length(const double *v)
 {
     return sqrt(v[0] * v[0] + v[1] * v[1] + v[2] * v[2]);
 }
 
-static inline
+STATIC_INLINE
 void add_vector(const double *a, const double *b, double *out)
 {
     for (int i = 0; i < 3; i++)
         out[i] = a[i] + b[i];
 }
 
-static inline
+STATIC_INLINE
 void subtract_vector(const double *a, const double *b, double *out)
 {
     for (int i = 0; i < 3; i++)
         out[i] = a[i] - b[i];
 }
 
-static inline
+STATIC_INLINE
 void multiply_vectors(const double *a, const double *b, double *out)
 {
     for (int i = 0; i < 3; i++)
         out[i] = a[i] * b[i];
 }
 
-static inline
+STATIC_INLINE
 void multiply_vector(const double *a, double b, double *out)
 {
     for (int i = 0; i < 3; i++)
         out[i] = a[i] * b;
 }
 
-static inline
+STATIC_INLINE
 void cross_product(const double *v1, const double *v2, double *out)
 {
     out[0] = v1[1] * v2[2] - v1[2] * v2[1];
@@ -58,7 +62,7 @@ void cross_product(const double *v1, const double *v2, double *out)
     out[2] = v1[0] * v2[1] - v1[1] * v2[0];
 }
 
-static inline
+STATIC_INLINE
 double dot_product(const double *v1, const double *v2)
 {
     double dp = 0.0;
@@ -67,7 +71,7 @@ double dot_product(const double *v1, const double *v2)
     return dp;
 }
 
-static inline
+STATIC_INLINE
 void scalar_triple_product(const double *u, const double *v, const double *w,
                            double *out)
 {
@@ -75,7 +79,7 @@ void scalar_triple_product(const double *u, const double *v, const double *w,
     multiply_vectors(u, out, out);
 }
 
-static inline
+STATIC_INLINE
 double scalar_triple(const double *u, const double *v, const double *w)
 {
     double tmp[3];
